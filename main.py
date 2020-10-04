7e83532ae45de4e6a3d703ebb0f4ae52950892a6
Lucas Zurverra <lzurverra@gmail.com>
2020-10-04 16:19:01 -0300

    packages list file and installation

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
+#!/usr/bin/env python3
 import json
-from os import system
+from os import system, path
+import os
+import tkinter as tk
+from tkinter import filedialog
 
-#system("sudo -s")
-archivo = open("output.txt", "w")
-with open(r"C:\Users\lzurverra\Desktop\packages.txt", "r") as json_file:
-    data = json.load(json_file)
-    for p in data:
-        package = p["name"]
-        archivo.write(package + "\n")
+DEFAULT_DIR = "/Users/lucas/Documents/develop/atom/atom-config/packages.json"
+FILEOPENOPTIONS = (("JSON files", "*.json"), ("All files", "*.*"))
 
-archivo.close()
+root = tk.Tk()
+root.withdraw()
+FILE_INPUT = filedialog.askopenfilename(initialdir=DEFAULT_DIR, title="Select file", filetypes=FILEOPENOPTIONS)
+FILE_OUTPUT = "packages.txt"
+
+with open(FILE_INPUT, "r") as json_file:
+    dir_path = os.path.dirname(json_file.name)
+    source = json.load(json_file)
+    packages = []
+    for index in range(len(source)):
+        package_name = source[index]["name"]
+        packages.append(package_name)
+    print(packages)
+json_file.close()
+
+with open(FILE_OUTPUT, "w") as output:
+    for package in packages:
+        output.write(package + "\n")
+output.close()
+
+system("sudo pip install -r {}".format(FILE_OUTPUT))
